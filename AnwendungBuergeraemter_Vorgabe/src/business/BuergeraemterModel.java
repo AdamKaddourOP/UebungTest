7e4a023a65e1c0210d69ef87e90676c15b866a70
Adamddour <106589116+Adamddour@users.noreply.github.com>
2023-12-03 21:48:20 +0100

    Montag

---
+++ AnwendungBuergeraemter_Vorgabe/src/business/BuergeraemterModel.java
@@ -0,0 +1,82 @@
+package business;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+
+import fabrikMethode.ConcreteCreator;
+import fabrikMethode.ConcreteCreatorTxt;
+import fabrikMethode.Creator;
+import fabrikMethode.Product;
+import ownUtil.Observable;
+import ownUtil.Observer;
+
+public class BuergeraemterModel implements Observable {
+	
+	private Buergeramt buergeramt;
+
+	public static BuergeraemterModel instanz=null;
+	LinkedList<Observer> list=new LinkedList<Observer>();
+	
+	public BuergeraemterModel() {
+		
+	}
+	
+	public static BuergeraemterModel getInstanz() {
+		
+		if(instanz==null) {
+			instanz=new BuergeraemterModel();
+		}
+		return instanz;
+	}
+	
+	public Buergeramt getBuergeramt() {
+		return this.buergeramt;
+	}
+	
+	public void setBuergeramt(Buergeramt buergeramt) {
+		this.buergeramt = buergeramt;
+		notifyObservers();
+	}
+ 		
+	public void schreibeBuergeraemterInCsvDatei()
+	    throws IOException{
+		Creator creator =new ConcreteCreator();
+		Product writer = creator.factoryMethod();
+		writer.fuegeInDateiHinzu(buergeramt);
+		writer.schliessDatei();
+
+ 	}
+	public void schreibeBuergeraemterInTxtDatei()    throws IOException{
+		
+		Creator creator =new ConcreteCreatorTxt();
+		Product writer = creator.factoryMethod();
+		writer.fuegeInDateiHinzu(buergeramt);
+		writer.schliessDatei();
+	}
+
+	@Override
+	public void addObserver(Observer obs) {
+		list.add(obs);
+		
+	}
+
+	@Override
+	public void removeObserver(Observer obs) {
+		list.remove(obs);
+		
+	}
+
+	@Override
+	public void notifyObservers() {
+		for(Observer o : list) {
+			
+			o.update();
+		}
+		
+	}
+
+	
+
+}
---
+++ AnwendungBuergeraemter_Vorgabe/src/gui/BuergeraemterControl.java
@@ -0,0 +1,52 @@
+package gui;
+
+import java.io.IOException;
+
+import business.BuergeraemterModel;
+import javafx.stage.Stage;
+import ownUtil.Observer;
+
+
+public class BuergeraemterControl implements Observer {
+	
+	
+	private BuergeraemterView buergeraemterView;
+	private BuergeraemterModel buergeraemterModel;
+
+	public BuergeraemterControl(Stage primaryStage){
+		this.buergeraemterModel =buergeraemterModel.getInstanz();
+		this.buergeraemterView = new BuergeraemterView(this, primaryStage,buergeraemterModel);
+		buergeraemterModel.addObserver(this);
+		
+	}
+	
+	void schreibeBuergeraemterInDatei(String typ){
+	   	try{
+	   		if("csv".equals(typ)){
+	   			buergeraemterModel.schreibeBuergeraemterInCsvDatei();
+	   			buergeraemterView.zeigeInformationsfensterAn("Die Aenderungen wurden gespeichert!");
+	   		}
+	   		else{
+	   			buergeraemterModel.schreibeBuergeraemterInTxtDatei();
+	   			buergeraemterView.zeigeInformationsfensterAn("Die Aenderungen wurden gespeichert!");
+	   		}
+	    } 
+		catch(IOException exc){
+			buergeraemterView.zeigeFehlermeldungsfensterAn("IOException beim Speichern!");
+		}
+		catch(Exception exc){
+			buergeraemterView.zeigeFehlermeldungsfensterAn("Unbekannter Fehler beim Speichern!");
+		}
+	   	
+    }
+
+	@Override
+	public void update() {
+		
+		buergeraemterView.zeigeBuergeraemterAn();
+		
+	}
+
+
+
+}
