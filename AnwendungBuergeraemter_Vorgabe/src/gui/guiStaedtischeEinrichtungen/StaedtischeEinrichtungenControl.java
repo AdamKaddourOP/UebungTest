a488c3113269f8c959e88f58dcb634623c4e8407
Adamddour <106589116+Adamddour@users.noreply.github.com>
2023-12-06 20:21:40 +0100

    Dienstag:2021

--- AnwendungBuergeraemter_Vorgabe/src/gui/guiStaedtischeEinrichtungen/StaedtischeEinrichtungenControl.java
+++ AnwendungBuergeraemter_Vorgabe/src/gui/guiStaedtischeEinrichtungen/StaedtischeEinrichtungenControl.java
@@ -7,18 +7,20 @@ import ownUtil.Observer;
 
 public class StaedtischeEinrichtungenControl implements Observer {
 
-	
+	//
 	private StaedtischeEinrichtungenView staedtischeEinrichtungenView;
 	private BuergeraemterModel buergeraemterModel;
 
 	public StaedtischeEinrichtungenControl(Stage primaryStage){
 		this.buergeraemterModel =buergeraemterModel.getInstanz();
+		//
 		this.staedtischeEinrichtungenView= new StaedtischeEinrichtungenView(this, primaryStage,buergeraemterModel);
 		//
 		buergeraemterModel.addObserver(this);
 		
 	}
 
+	//
 	@Override
 	public void update() {
 		staedtischeEinrichtungenView.zeigeBuergeraemterAn();
